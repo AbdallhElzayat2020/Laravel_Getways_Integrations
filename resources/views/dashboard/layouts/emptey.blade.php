f5e6ebf58e94bbd54a7dc01a5e6f903ab627654e
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2024-06-04 07:30:43 +0300

    paypal_getway

---
+++ resources/views/dashboard/layouts/emptey.blade.php
@@ -0,0 +1,10 @@
+@extends('dashboard.layouts.master')
+@section('title')
+    Title Page
+@endsection
+@section('css')
+@endsection
+@section('content')
+@endsection
+@section('js')
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+    /*
+    |--------------------------------------------------------------------------
+    | Web Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register web routes for your application. These
+    | routes are loaded by the RouteServiceProvider and all of them will
+    | be assigned to the "web" middleware group. Make something great!
+    |
+    */
+
+    use App\Http\Controllers\Dashboard\SettingController;
+    use Illuminate\Support\Facades\Route;
+    
+    Route::get('/' , function () {
+        return view('dashboard.index');
+    });
+
+    Route::prefix('dashboard')->group(function () {
+        Route::get('/settings' , function () {
+            return view('dashboard.settings');
+            // echo 'Settings';
+        })->name('dashboard.settings');
+        Route::post('/settings/update' , [ SettingController::class , 'update' ])->name('setting.update');
+    });
